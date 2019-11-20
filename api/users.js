96ce1c431388385d665a1ca0a076f7d52683bff7
Deven Swiergiel <Deven.Swiergiel@its.ny.gov>
2019-11-20 08:31:47 -0500

    Makes directory change for deployment

---
+++ api/users.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
+
+// @route   POST api/users
+// @desc    Register a user
+// @access  Public
+router.post(
+  '/',
+  [
+    // express model validation
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    // check if validation errors were returned
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Object destructuring(part of ES6)
+    const { name, email, password } = req.body;
+
+    try {
+      // check if user with that email already exists in db
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+
+      // create new User object
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      // take plaintext password and hash it with bcrypt
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // return user paylod
+      // only id is needed to fetch user from db in the future so that's what sent
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      // create jwt based on payload
+      // expire token(require login again) in 360000
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,16 +6,19 @@ const connectDB = require('./config/db.js');
 connectDB();
 
 // Init Middleware
-app.use(express.json({extended: false}));
+app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper API' }));
-
+app.get('/', (req, res) =>
+  res.json({ msg: 'Welcome to the ContactKeeper API' })
+);
 
 // seperated endpoints to their respsctive files and using routes to handle
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/users', require('./api/users'));
+app.use('/api/auth', require('./api/auth'));
+app.use('/api/contacts', require('./api/contacts'));
 
-app.listen(PORT, () => console.log(`Server running at: http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running at: http://localhost:${PORT}`)
+);
